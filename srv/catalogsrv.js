31deaf7deb34a1049597c53a695941ea12720220
Sachin Badnikai <sachinbadnikai143@gmail.com>
2026-02-25 05:52:43 +0000

    adding some code for revert

--- srv/catalogsrv.js
+++ srv/catalogsrv.js
@@ -12,3 +12,15 @@ module.exports = srv =>{
         console.log("*****************************",result)
     })
 }
+
+module.exports = srv =>{
+
+    srv.on('READ', 'TestSet', async(req)=>{
+        console.log("************************hello*****",req.params)
+        const ID =req.data.ID;
+         result = await cds.db.run(
+  SELECT.from(Test).where({ ID })
+); 
+        console.log("*****************************",result)
+    })
+}
