bebcfaded9b532eb1f79d7f3b6eb90572e45793e
Sachin Badnikai <sachinbadnikai143@gmail.com>
2026-02-25 12:40:58 +0000

    jhjh

--- srv/catalogsrv.js
+++ srv/catalogsrv.js
@@ -2,7 +2,7 @@ const cds =require('@sap/cds');
 const { Test } = cds.entities; 
 ***************sachin
 ***************sachin
-***************sachin
+***************sachinkkkkkkkkkkkkkk
 module.exports = srv =>{
 *****************************************sachin
     srv.on('READ', 'TestSet', async(req)=>{
