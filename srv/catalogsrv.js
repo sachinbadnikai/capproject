ddce7f0a712af61afe62959d5a9cf45b26ac64e9
Sachin Badnikai <sachinbadnikai143@gmail.com>
2026-02-26 07:17:55 +0000

    sdsdsd

--- srv/catalogsrv.js
+++ srv/catalogsrv.js
@@ -13,7 +13,7 @@ module.exports = srv =>{
 
     srv.on('READ', 'TestSet', async(req)=>{
         console.log("************************hello*****",req.params)
-        asas 
+        asas  test8
         main main
         const ID =req.data.ID;
          result = await cds.db.run(
